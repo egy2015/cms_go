ba2618c64fb7ace5557a60b8097de219e9cbaebc
Dicky Wahyudi Pratama <dicky.pratama@ottodigital.id>
2024-06-06 02:12:27 +0700

    Router Init

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"Egy2015/cms_go/router"
+
+	_ "github.com/joho/godotenv/autoload"
+)
+
+func main() {
+	router.Run()
+}
---
+++ router/cors.go
@@ -0,0 +1,9 @@
+package router
+
+import "github.com/gin-contrib/cors"
+
+var corsConfig = cors.Config{
+	AllowAllOrigins: true,
+	AllowHeaders:    []string{"Authorization", "Content-Type"},
+	AllowMethods:    []string{"DELETE", "GET", "POST", "PUT"},
+}
